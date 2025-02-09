8f5c331bd6fc3b5c38b69e7f3b555a580213a10b
lamyafj <lamyafahadj@gmail.com>
2025-02-09 22:49:51 +0300

    suitcase

--- src/app/terhalsuitcase.tsx
+++ src/app/terhalsuitcase.tsx
@@ -1,16 +1,19 @@
 "use client";
+import "@google/model-viewer";
 
-//import { useLanguage } from "./language";
-
-
-const TerhalSuitcase = () => {
-
+export default function TerhalSuitcase() {
   return (
-    <div className=" min-h-[500px]" >
-
-terhal suitcase section
-    </div>
+    <model-viewer
+      src="./suitcase1.glb"
+      alt="3D Suitcase"
+      auto-rotate
+      camera-controls
+      style={{ width: "100%", height: "500px" }}
+    ></model-viewer>
   );
-};
+}
 
-export default TerhalSuitcase;
+
+
+
+// export default TerhalSuitcase;
